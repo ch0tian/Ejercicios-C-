af4bd09b2e581fe7c697d9119ace7196887b0e74
Ch0tian <80105497+ch0tian@users.noreply.github.com>
2024-11-13 02:08:48 -0600

    Add files via upload
    Escribe el código en C# que permita solucionar el siguiente problema:
    Implementar un programa, que dados tres números (a,b y c), muestre un menú con las opciones:
    1.	Validar suma
    2.	 Salir
    
    *Si se elige la primer opción, mostrar el mensaje “SON IGUALES” si la suma de dos de ellos (a+b, b+c o a+c) es igual al número restante y el mensaje “SON DISTINTOS” en caso contrario. 
    
    *Si se elige la segunda opción, mostrar un mensaje de despedida y terminar el programa.
    Incluir la cláusula default para indicar al usuario si eligió una opción no válida.

---
+++ Actividad 3-Uso-IF-Case-EstructuraDeDatos/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+public class Programa
+{
+    public static void Main()
+    {
+        // Aquí se ingresan los 3 valores
+        Console.Write("Introducir el valor de a: ");
+        int a = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Introducir el valor de b: ");
+        int b = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Introducir el valor de c: ");
+        int c = Convert.ToInt32(Console.ReadLine());
+
+        while (true)
+        {
+            // Aquí se muestra el menú
+            Console.WriteLine("Menú:");
+            Console.WriteLine("1. Validar suma");
+            Console.WriteLine("2. Salir");
+            Console.Write("Elegir una opción: ");
+            int opcion = Convert.ToInt32(Console.ReadLine());
+
+            switch (opcion)
+            {
+                case 1:
+                    if ((a + b == c) || (a + c == b) || (b + c == a))
+                    {
+                        Console.WriteLine("Son iguales");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Son distintos");
+                    }
+                    break;
+
+                case 2:
+                    Console.WriteLine("¡Hasta luego!");
+                    return; // Aquí termina el programa
+
+                default:
+                    Console.WriteLine("Opción incorrecta, por favor inténtalo de nuevo");
+                    break;
+            }
+        }
+    }
+}
